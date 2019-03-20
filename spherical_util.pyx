54cd28bec40d85fd66d4ca9d175613a87ec459ea
Stephan vdW <stephan.vdwildenberg@gmail.com>
2019-03-20 17:06:06 +0100

    made the shit you needed me bteween you qnd the keyboard for

--- spherical_util.pyx
+++ spherical_util.pyx
@@ -8,9 +8,10 @@ cdef extern from "spherical_util.h":
     void spherical_harmonics(double *thet,double *phi,int *l,int *ml,double *result,int length)
     void cart_to_spher(double *x,double *y,double *z,double * r,double *t,double *f,int length)
     void density_cont_val(double* reorthog,double *imorthog,double *x,double*y,double*z,double*kx,double*ky,double*kz,int nx,int ny,int nz,int nk,double* dens_val)
-    void build_transition_density_matrix(int n_states_neut,int n_closed,int n_occ,int ci_size_neut,int n_elec_neut,double *ci_vec_neut,double *tran_den_mat_mo)
+    void build_transition_density_matrix(int n_states_neut,int n_closed,int n_occ,int ci_size_neut,int n_elec_neut,double *ci_vector,int *mos_vector,int* spin_vector,double *tran_den_mat_mo)
 
-def pbuild_transition_density_matrix(i,l,m,x,y,
+def pbuild_transition_density_matrix(i,l,m,x,y,np.ndarray[double, ndim=1,mode="c"] civec,np.ndarray[double, ndim=1,mode="c"] mos_vec,np.ndarray[double, ndim=1,mode="c"]spin_vec,np.ndarray[double, ndim=1,mode="c"] tdmmo):
+    build_transition_density_matrix(i,l,m,x,y,&civec[0],&mos_vec[0],&spin_vec[0],&tdmmo[0])
 
 def pspher_harmo(np.ndarray[double, ndim=1,mode="c"] thet,np.ndarray[double, ndim=1,mode="c"] phi,np.ndarray[ int, ndim=1,mode="c"] l,np.ndarray[int, ndim=1,mode="c"] ml,np.ndarray[double, ndim=1,mode="c"] result):
 
--- spherical_utils.cpp
+++ spherical_utils.cpp
@@ -138,7 +138,7 @@ void density_cont_val(double* reorthog,double* imorthog,double *x,double*y,doubl
    return ;
 }
 
-void build_transition_density_matrix(int n_states_neut,int n_closed,int n_occ,int ci_size_neut,int n_elec_neut,double *ci_vec_neut,double *tran_den_mat_mo)
+void build_transition_density_matrix(int n_states_neut,int n_closed,int n_occ,int ci_size_neut,int n_elec_neut,double *ci_vector,int *mos_vector,int* spin_vector,double *tran_den_mat_mo)
 {
 
    bool test(0);
@@ -166,21 +166,21 @@ void build_transition_density_matrix(int n_states_neut,int n_closed,int n_occ,in
                   for(int n=0;n!=ci_size_neut;n++)
                   {
 
-                     det_val=build_reduced_determinant(k,kp,n_elec_neut,n_closed,n_occ,&ci_vec_neut[(n_elec_neut+n_states_neut)*m],&ci_vec_neut[(n_elec_neut+n_states_neut)*n],&ci_vec_neut[1][n_elec_neut*m],&ci_vec_neut[1][n_elec_neut*n]);
+                     det_val=build_reduced_determinant(k,kp,n_elec_neut,n_closed,n_occ,&mos_vector[m*n_elec_neut],&mos_vector[n_elec_neut*n],&spin_vector[n_elec_neut*m],&spin_vector[n_elec_neut*n]);
 
                      //tran_den_mat_mo[i*n_states_neut+j][k*(n_occ+n_closed)+kp]+=ci_vec_neut[0][(n_elec_neut+n_states_neut)*(m)+n_elec_neut+i]*ci_vec_neut[0][(n_elec_neut+n_states_neut)*(n)+n_elec_neut+j]*det_val;
-                     tran_den_mat_mo[i*n_states_neut*(n_occ+n_closed)*(n_occ+n_closed)+j*(n_occ+n_closed)*(n_occ+n_closed)+k*(n_occ+n_closed)+kp]+=ci_vec_neut[(n_elec_neut+n_states_neut)*(m)+n_elec_neut+i]*ci_vec_neut[(n_elec_neut+n_states_neut)*(n)+n_elec_neut+j]*det_val;
+                     tran_den_mat_mo[i*n_states_neut*(n_occ+n_closed)*(n_occ+n_closed)+j*(n_occ+n_closed)*(n_occ+n_closed)+k*(n_occ+n_closed)+kp]+=ci_vector[n_states_neut*m+i]*ci_vector[n_states_neut*n+j]*det_val;
                    }
                  }
-               if(k==kp)
-               {
-                 sum+=tran_den_mat_mo[i*n_states_neut+j][k*(n_occ+n_closed)+kp];
+//               if(k==kp)
+//               {
+//                 sum+=tran_den_mat_mo[i*n_states_neut*(n_occ+n_closed)*(n_occ+n_closed)+j*(n_occ+n_closed)*(n_occ+n_closed)+k*(n_occ+n_closed)+kp];
                //  std::cout<<" from orbital "<<k<<" and from orbital "<<kp<<":"<<tran_den_mat_mo[i*n_states_neut+j][k*(n_occ+n_closed)+kp]<<std::endl;
-               }
+//               }
 //               std::cout<<std::setprecision(8)<<"trdm val "<<tran_den_mat_mo[i*n_states_neut+j][k*(n_occ+n_closed)+kp]<<std::endl;
               }
            }
-         std::cout<<"SUM = "<<sum<<std::endl;
+//         std::cout<<"SUM = "<<sum<<std::endl;
         }
     }
 }
