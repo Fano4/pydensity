b00386a62eb33a5a5eaa4fefbc808fe3056de8ea
Acu-remaclecluster <acuzzio@gmail.com>
2019-08-29 19:07:52 +0200

    selector

--- create_gridlists.py
+++ create_gridlists.py
@@ -6,6 +6,16 @@ import os
 import quantumpropagator as qp
 from tqdm import tqdm
 
+def points_in_sphere(pt1, r, q):
+    '''
+    given a 1d vector of points q, returns a boolean telling if the point is into the
+    sphere centred at pt1 and radius r.
+    '''
+
+    vec = pt1 - q
+    norm = np.linalg.norm(vec, axis=1)
+    boolean1 = np.abs(norm) < r
+    return boolean1
 
 def points_in_cylinder(pt1, pt2, r, q, cyl_shrink):
     '''
@@ -13,11 +23,21 @@ def points_in_cylinder(pt1, pt2, r, q, cyl_shrink):
     cylinder defined by the two points pt1 and pt2 and radius r.
     cyl_shrink is the distance between the two atoms at which the cylinder starts
     '''
+
     vec = pt2 - pt1
-    const = r * np.linalg.norm(vec)
-    boolean1 = np.dot(q - pt1, vec) >= cyl_shrink
-    boolean2 = np.dot(q - pt2, vec) <= cyl_shrink
-    boolean3 = np.linalg.norm(np.cross(q - pt1, vec), axis=1) <= const
+    vec_norm = np.linalg.norm(vec)
+    versor = vec / vec_norm
+    pt1_2 = pt1 + (versor * cyl_shrink)
+    pt2_2 = pt1 + (versor * (vec_norm-cyl_shrink))
+
+    vec2 = pt2_2 - pt1_2
+    vec2_norm = np.linalg.norm(vec2)
+
+    const = r * vec2_norm
+    boolean1 = np.dot(q - pt1_2, vec2) >= 0
+    boolean2 = np.dot(q - pt2_2, vec2) <= 0
+    boolean3 = np.linalg.norm(np.cross(q - pt1_2, vec2), axis=1) <= const
+
     return boolean1*boolean2*boolean3
 
 def main(geoms):
@@ -28,8 +48,8 @@ def main(geoms):
     tre = 8
     qua = 7
 
-    r = 0.6
-    cyl_shrink = 0.6
+    r = 1.0 # BOHR
+    cyl_shrink = 1.0 # BOHR
 
     xmin, ymin, zmin = -10,-10,-10
     nx, ny, nz = 64,64,64
@@ -44,8 +64,10 @@ def main(geoms):
     list_of_points_in_3d = np.stack([A.flatten(),B.flatten(),C.flatten()]).T
     print(list_of_points_in_3d.shape)
 
-    list_first = np.empty(pL*gL*tL,dtype=object)
-    list_second = np.empty(pL*gL*tL,dtype=object)
+    list_1 = np.empty(pL*gL*tL,dtype=object)
+    list_2 = np.empty(pL*gL*tL,dtype=object)
+    list_3 = np.empty(pL*gL*tL,dtype=object)
+    list_4 = np.empty(pL*gL*tL,dtype=object)
     counter = 0
     for p in tqdm(range(pL)[:]):
         for g in tqdm(range(gL)[:], leave=False):
@@ -60,18 +82,41 @@ def main(geoms):
                 b = np.where(points_in_cylinder(pt3, pt4, r, list_of_points_in_3d, cyl_shrink))
                 c = np.where(points_in_cylinder(pt1, pt4, r, list_of_points_in_3d, cyl_shrink))
                 d = np.where(points_in_cylinder(pt2, pt3, r, list_of_points_in_3d, cyl_shrink))
+                e = np.where(points_in_sphere(pt1, r, list_of_points_in_3d))
+                f = np.where(points_in_sphere(pt2, r, list_of_points_in_3d))
 
-                list_first[counter] = np.concatenate((a[0],b[0]))
-                list_second[counter] = np.concatenate((c[0],d[0]))
-                if counter % 1000000 == 'aalle': # one each 1000 (I do not want this to trigger)
+                list_1[counter] = np.concatenate((a[0],b[0]))
+                list_2[counter] = np.concatenate((c[0],d[0]))
+                list_3[counter] = e[0]
+                list_4[counter] = f[0]
+
+                if counter % 1000000 == 10: # one each 1000 (I do not want this to trigger)
                     first_thing = qp.fromBohToAng(np.concatenate((list_of_points_in_3d[a],list_of_points_in_3d[b])))
                     second_thing = qp.fromBohToAng(np.concatenate((list_of_points_in_3d[c],list_of_points_in_3d[d])))
+                    fourth_thing = qp.fromBohToAng(list_of_points_in_3d[e])
+                    fifth_thing = qp.fromBohToAng(list_of_points_in_3d[f])
+
+                    #DEBUG
+                    vec = pt2 - pt1
+                    vec_norm = np.linalg.norm(vec)
+                    versor = vec / vec_norm
+                    pt1_2 = pt1 + (versor * cyl_shrink)
+                    pt2_2 = pt1 + (versor * (vec_norm-cyl_shrink))
+                    third_thing = qp.fromBohToAng(np.stack([pt1,pt2,pt1_2,pt2_2]))
+                    qp.saveTraj(np.array([third_thing]),['H']*len(third_thing),'cayo{}'.format(counter))
+                    ######
+
                     qp.saveTraj(np.array([single]),['C','C','C','H','H','H','H','C','C','C','C','H','H','H','H'], '{}'.format(counter),convert=True)
                     qp.saveTraj(np.array([first_thing]),['H']*len(first_thing),'poi{}'.format(counter))
+                    qp.saveTraj(np.array([second_thing]),['H']*len(second_thing),'poiT{}'.format(counter))
+                    qp.saveTraj(np.array([fourth_thing]),['H']*len(fourth_thing),'carbon{}'.format(counter))
+                    qp.saveTraj(np.array([fifth_thing]),['H']*len(fifth_thing),'carbonT{}'.format(counter))
                 counter+=1
     dic2 = {}
-    dic2['list_first'] = list_first
-    dic2['list_second'] = list_second
+    dic2['list_1'] = list_1
+    dic2['list_2'] = list_2
+    dic2['list_3'] = list_3
+    dic2['list_4'] = list_4
     qp.pickleSave('first_second.p',dic2)
     # this file now contains two lists. Each one of those is 65000 long. In each of those 65000 there is a list of grid points in the flattened list. 
     # We need to pass those grid points to the routine of density, so we can sum up...
